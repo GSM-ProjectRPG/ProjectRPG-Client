ae90327691d7e00e081be532d4f7b5a318424154
hsh0517 <pelino0517@gmail.com>
2023-07-16 19:13:31 +0900

    :hammer: :: PacketHandler 클래스 추가

---
+++ Assets/ProjectRPG/Scripts/Network/Packet/PacketHandler.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ACore;
+using Google.Protobuf;
+using Google.Protobuf.Protocol;
+
+public class PacketHandler
+{
+    public static void S_ConnectedToServerHandler(PacketSession session, IMessage packet)
+    {
+        // TODO : 로그인 시작
+    }
+}
