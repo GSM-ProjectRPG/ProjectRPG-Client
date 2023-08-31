a5f1268bc51af99794a3c6a1ae91af3af5627b6c
hsh0517 <pelino0517@gmail.com>
2023-08-31 13:52:38 +0900

    :sparkles: :: PacketHandler 함수들 추가

--- Assets/ProjectRPG/Scripts/Network/Packet/PacketHandler.cs
+++ Assets/ProjectRPG/Scripts/Network/Packet/PacketHandler.cs
@@ -11,4 +11,84 @@ public class PacketHandler
     {
         // TODO : 로그인 시작
     }
+
+    public static void S_LoginHandler(PacketSession session, IMessage packet)
+    {
+
+    }
+
+    public static void S_CreatePlayerHandler(PacketSession session, IMessage packet)
+    {
+
+    }
+
+    public static void S_EnterGameHandler(PacketSession session, IMessage packet)
+    {
+
+    }
+
+    public static void S_LeaveGameHandler(PacketSession session, IMessage packet)
+    {
+
+    }
+
+    public static void S_SpawnHandler(PacketSession session, IMessage packet)
+    {
+
+    }
+
+    public static void S_DespawnHandler(PacketSession session, IMessage packet)
+    {
+
+    }
+
+    public static void S_MoveHandler(PacketSession session, IMessage packet)
+    {
+
+    }
+
+    public static void S_SkillHandler(PacketSession session, IMessage packet)
+    {
+
+    }
+
+    public static void S_ChangeHpHandler(PacketSession session, IMessage packet)
+    {
+
+    }
+
+    public static void S_DieHandler(PacketSession session, IMessage packet)
+    {
+
+    }
+
+    public static void S_ItemListHandler(PacketSession session, IMessage packet)
+    {
+
+    }
+
+    public static void S_AddItemHandler(PacketSession session, IMessage packet)
+    {
+
+    }
+
+    public static void S_EquipItemHandler(PacketSession session, IMessage packet)
+    {
+
+    }
+
+    public static void S_ChangeStatHandler(PacketSession session, IMessage packet)
+    {
+
+    }
+
+    public static void S_ChatHandler(PacketSession session, IMessage packet)
+    {
+
+    }
+
+    public static void S_PingHandler(PacketSession session, IMessage packet)
+    {
+
+    }
 }
