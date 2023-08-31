5ef9a73308785102f200e679ccc30178ac2ca463
hsh0517 <pelino0517@gmail.com>
2023-08-31 16:53:28 +0900

    :sparkles: :: WebPacket 추가

---
+++ Assets/ProjectRPG/Scripts/Network/Web/WebPacket.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+
+namespace ProjectRPG
+{
+    public class CreateAccountPacketReq
+    {
+        public string AccountName;
+        public string Password;
+    }
+
+    public class CreateAccountPacketRes
+    {
+        public bool CreateOk;
+    }
+
+    public class LoginAccountPacketReq
+    {
+        public string AccountName;
+        public string Password;
+    }
+
+
+    public class ServerInfo
+    {
+        public string Name;
+        public string IpAddress;
+        public int Port;
+        public int BusyScore;
+    }
+
+    public class LoginAccountPacketRes
+    {
+        public bool LoginOk;
+        public int AccountId;
+        public int Token;
+        public List<ServerInfo> ServerList = new List<ServerInfo>();
+    }
+}
