f57eace1efc39c6045eeabed3fe1a6d6e0b4fd80
hsh0517 <pelino0517@gmail.com>
2023-09-11 13:55:56 +0900

    :sparkles: :: NetworkManager 작성

--- Assets/ProjectRPG/Scripts/Manager/Contents/NetworkManager.cs
+++ Assets/ProjectRPG/Scripts/Manager/Contents/NetworkManager.cs
@@ -1,14 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System.Net;
+using ACore;
+using Google.Protobuf;
 
 namespace ProjectRPG
 {
     public class NetworkManager
     {
+        public int AccountId { get; set; }
+        public int Token { get; set; }
+
+        private ServerSession _session = new ServerSession();
+
+        public void Send(IMessage packet)
+        {
+            _session.Send(packet);
+        }
+
+        public void Connect(ServerInfo info)
+        {
+            var ipAddr = IPAddress.Parse(info.IpAddress);
+            var endPoint = new IPEndPoint(ipAddr, info.Port);
+            var connector = new Connector();
+            connector.Connect(endPoint, () => _session, 1);
+        }
+
         public void Update()
         {
-
+            var list = PacketQueue.Instance.PopAll();
+            foreach (var packet in list)
+            {
+                var handler = PacketManager.Instance.GetPacketHandler(packet.Id);
+                handler?.Invoke(_session, packet.Message);
+            }
         }
     }
 }
