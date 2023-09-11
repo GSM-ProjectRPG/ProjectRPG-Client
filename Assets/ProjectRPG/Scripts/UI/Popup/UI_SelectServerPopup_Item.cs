347564f10db808d3add680b8b5caefa4080814f3
hsh0517 <pelino0517@gmail.com>
2023-09-11 14:06:17 +0900

    :sparkles: :: UI_SelectServerPopup_Item 추가

---
+++ Assets/ProjectRPG/Scripts/UI/Popup/UI_SelectServerPopup_Item.cs
@@ -0,0 +1,43 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+
+namespace ProjectRPG
+{
+    public class UI_SelectServerPopup_Item : UI_Base
+    {
+        public ServerInfo Info { get; set; }
+
+        private enum Buttons
+        {
+            SelectServerButton
+        }
+
+        private enum Texts
+        {
+            ServerNameText
+        }
+
+        public override void Init()
+        {
+            Bind<Button>(typeof(Buttons));
+            Bind<TMP_Text>(typeof(Texts));
+
+            GetButton((int)Buttons.SelectServerButton).gameObject.BindEvent(OnClickButton);
+        }
+
+        public void RefreshUI()
+        {
+            if (Info == null) return;
+
+            GetText((int)Texts.ServerNameText).text = Info.Name;
+        }
+
+        private void OnClickButton(PointerEventData evt)
+        {
+            Managers.Network.Connect(Info);
+            Managers.Scene.LoadScene(Define.Scene.Game);
+            Managers.UI.ClosePopupUI();
+        }
+    }
+}
