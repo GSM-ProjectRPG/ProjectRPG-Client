a2e860c8e9b9009af4326ff8e1d3521959c185b0
hsh0517 <pelino0517@gmail.com>
2023-09-27 09:07:11 +0900

    :sparkles: :: DataManager 작성

--- Assets/ProjectRPG/Scripts/Manager/Core/DataManager.cs
+++ Assets/ProjectRPG/Scripts/Manager/Core/DataManager.cs
@@ -10,12 +10,16 @@ namespace ProjectRPG
 
     public class DataManager
     {
-        // TODO : Data Dictionary
+        public Dictionary<int, Data.Skill> SkillDict { get; private set; } = new Dictionary<int, Data.Skill>();
+        public Dictionary<int, Data.ItemData> ItemDict { get; private set; } = new Dictionary<int, Data.ItemData>();
+        public Dictionary<int, Data.MonsterData> MonsterDict { get; private set; } = new Dictionary<int, Data.MonsterData>();
 
-	    public void Init()
+        public void Init()
         {
-            // TODO : LoadJson And MakeDict
-	    }
+            SkillDict = LoadJson<Data.SkillData, int, Data.Skill>("SkillData").MakeDict();
+            ItemDict = LoadJson<Data.ItemLoader, int, Data.ItemData>("ItemData").MakeDict();
+            MonsterDict = LoadJson<Data.MonsterLoader, int, Data.MonsterData>("MonsterData").MakeDict();
+        }
 
         private Loader LoadJson<Loader, Key, Value>(string path) where Loader : ILoader<Key, Value>
         {
